fe04ccd4fcfdda6dd013a952bc07708816360f42
Dan Borufka <dan@polygoat.org>
2017-11-24 19:14:33 +0100

    Cleaning template parts and creating first section type

--- wp-content/themes/wp_gulp/template-parts/title-bar_part.php
+++
@@ -1,9 +0,0 @@
-
-	<?php $subheadline = get_field('subheadline'); ?>
-
-	<div class="titleBar">
-		<div class="titles">
-			<h1><?php echo $title ?></h1>	
-			<h2><?php echo $subheadline ?></h2>
-		</div>
-	</div>
