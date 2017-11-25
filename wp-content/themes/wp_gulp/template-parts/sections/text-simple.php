57db3ea4f702086f7a22d2febcec25883b6ca9de
Dan <dan@MWMP.local>
2017-11-25 21:49:32 +0100

    Styling adaptions…

--- wp-content/themes/wp_gulp/template-parts/sections/text-simple.php
+++ wp-content/themes/wp_gulp/template-parts/sections/text-simple.php
@@ -17,12 +17,12 @@
 
 
 		<?php if(get_sub_field('img_left')): ?>
-			<div class="image-container cell small-3">
+			<div class="image-container cell small-12 medium-3">
 				<img src="<?= get_sub_field('img_left')['url']; ?>" width=150>
 			</div>
 		<?php endif; ?>
 
-		<p class="cell small-7">
+		<p class="<?php if(get_sub_field('img_left')): ?>cell small-12 medium-9<?php endif; ?>">
 			<?php the_sub_field('content'); ?>
 		</p>
 	</div>
