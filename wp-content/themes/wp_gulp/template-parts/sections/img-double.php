a9299e6adb8a75e03d65f3df1aeee07fc5348099
Dan Borufka <dan@polygoat.org>
2018-01-14 16:51:09 +0100

    Applying same hack to double pics

--- wp-content/themes/wp_gulp/template-parts/sections/img-double.php
+++ wp-content/themes/wp_gulp/template-parts/sections/img-double.php
@@ -11,10 +11,14 @@
 <div class="section cell img-block img-double">
 	<div class="grid-x grid-padding-x wide">
 		<div class="cell medium-6 small-12">
-			<div class="image" style="background-image:url('<?php echo get_sub_field('image-left')['url']; ?>');"></div>
+			<div class="image" style="background-image:url('<?php echo get_sub_field('image-left')['url']; ?>');">
+				<img src="<?php echo get_sub_field('image-left')['url']; ?>">
+			</div>
 		</div>
 		<div class="cell medium-6 small-12">
-			<div class="image" style="background-image:url('<?php echo get_sub_field('image-right')['url']; ?>');"></div>
+			<div class="image" style="background-image:url('<?php echo get_sub_field('image-right')['url']; ?>');">
+				<img src="<?php echo get_sub_field('image-right')['url']; ?>">
+			</div>
 		</div>
 	</div>
 </div>
