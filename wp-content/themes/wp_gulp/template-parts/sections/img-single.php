299a4214a460fc41f8e167487a6faaf7aa605947
Dan Borufka <dan@polygoat.org>
2018-01-14 16:49:25 +0100

    Adding CSS hack for images

--- wp-content/themes/wp_gulp/template-parts/sections/img-single.php
+++ wp-content/themes/wp_gulp/template-parts/sections/img-single.php
@@ -10,6 +10,8 @@
 ?>
 <div class="section cell img-block img-single">
 	<div class="grid-container grid-x">
-		<div class="image cell small-12" style="background-image:url('<?php echo get_sub_field('image')['url']; ?>'); <?php if(get_sub_field('forced_height')): ?>height:<?php the_sub_field('forced_height'); ?>px; max-height:<?php the_sub_field('forced_height'); ?>px;<?php endif; ?>"></div>
+		<div class="image cell small-12" style="background-image:url('<?php echo get_sub_field('image')['url']; ?>'); <?php if(get_sub_field('forced_height')): ?>height:<?php the_sub_field('forced_height'); ?>px; max-height:<?php the_sub_field('forced_height'); ?>px;<?php endif; ?>">
+			<img src="<?php echo get_sub_field('image')['url']; ?>">
+		</div>
 	</div>
 </div>
