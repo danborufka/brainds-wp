0e76e1112bb1e454800cb87323034ac5d1828555
Dan Borufka <dan@polygoat.org>
2018-01-29 11:03:50 +0100

    Forcing image size 'large' on template double-images

--- wp-content/themes/wp_gulp/template-parts/sections/img-double.php
+++ wp-content/themes/wp_gulp/template-parts/sections/img-double.php
@@ -11,13 +11,13 @@
 <div class="section cell img-block img-double">
 	<div class="grid-x grid-padding-x wide">
 		<div class="cell medium-6 small-12">
-			<div class="image" style="background-image:url('<?php echo get_sub_field('image-left')['url']; ?>');">
-				<img src="<?php echo get_sub_field('image-left')['url']; ?>">
+			<div class="image" style="background-image:url('<?php echo get_sub_field('image-left')['sizes']['large']; ?>');">
+				<img src="<?php echo get_sub_field('image-left')['sizes']['large']; ?>">
 			</div>
 		</div>
 		<div class="cell medium-6 small-12">
-			<div class="image" style="background-image:url('<?php echo get_sub_field('image-right')['url']; ?>');">
-				<img src="<?php echo get_sub_field('image-right')['url']; ?>">
+			<div class="image" style="background-image:url('<?php echo get_sub_field('image-right')['sizes']['large']; ?>');">
+				<img src="<?php echo get_sub_field('image-right')['sizes']['large']; ?>">
 			</div>
 		</div>
 	</div>
